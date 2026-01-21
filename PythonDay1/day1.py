60d0d5854c73d66e51ca5a89514696a7d29ac9d9
Gertinjo <gerticalaj2013@gmail.com>
2026-01-21 19:09:15 +0100

    sadsadsasad

---
+++ PythonDay1/day1.py
@@ -0,0 +1,28 @@
+# print('Hello World')
+# print('!')
+
+# x= 12
+# y= 11
+# g = x-y
+# print(g)
+
+
+first_name="Gerti"
+surname="Calaj"
+Fullname = first_name  +  surname
+print(Fullname)
+
+sports = ["Football" , "Tenis" , "Basketball"]
+print(sports)
+sports.append("Golf")
+print(sports)
+sports.remove(sports[1])
+print(sports)
+
+# names = ["Gerti Calaj" , "Dren Gashi"]
+# for name in names:
+#     print(name)
+
+# numbers = [ 1 , 2 , 3]
+# for number in numbers:
+#     print(number)
